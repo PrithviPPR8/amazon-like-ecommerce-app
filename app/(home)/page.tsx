83295d39984e6ea219711c658bf2409e2ae294e7
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-09-02 14:01:04 +0530

    4 Created Home Page Carousels

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,8 @@
-
+import HomeCarousel from "@/components/shared/home/HomeCarousel";
+import data from "@/lib/data";
 
 export default async function Page() {
   return (
-    <div className="h1-bold text-center p-10">
-      <h1>Home Page Content</h1>
-    </div>
+    <HomeCarousel items={data.carousels} />
   );
 }
