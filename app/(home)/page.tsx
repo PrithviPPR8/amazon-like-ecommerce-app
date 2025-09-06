f8db100c93b5a3a6cfbb41bb55c19c86dc7f8e60
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-09-06 12:28:10 +0530

    8 Create Best Selling Slider

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -70,6 +70,7 @@ export default async function Page() {
   ]
 
   const todaysDeals = await getProductsByTag({ tag: "todays-deal" });
+  const bestSellingProducts = await getProductsByTag({ tag: "best-seller" });
 
   return (
     <>
@@ -79,6 +80,16 @@ export default async function Page() {
         <Card>
           <CardContent>
             <ProductSlider title={"Today's Deals"} products={todaysDeals} />
+          </CardContent>
+        </Card>
+
+        <Card className='w-full rounded-none'>
+          <CardContent className='p-4 items-center gap-3'>
+            <ProductSlider
+              title={"Best Selling Products"}
+              products={bestSellingProducts}
+              hideDetails
+            />
           </CardContent>
         </Card>
       </div>
