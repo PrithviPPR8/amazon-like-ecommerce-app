7cbc019dbc0c9edb12021168d049f2bb6f49ed38
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-09-09 21:42:14 +0530

    14 Sign In User functionality added

--- types/index.ts
+++ types/index.ts
@@ -2,6 +2,8 @@ import {
   CartSchema,
   OrderItemSchema,
   ProductInputSchema,
+  UserInputSchema,
+  UserSignInSchema,
 } from '@/lib/validator'
 import z from "zod";
 
@@ -11,7 +13,7 @@ export type IProductInput = z.infer<typeof ProductInputSchema>
 export type Data = {
 //   settings: ISettingInput[]
 //   webPages: IWebPageInput[]
-//   users: IUserInput[]
+  users: IUserInput[]
   products: IProductInput[]
 //   reviews: {
 //     title: string
@@ -33,3 +35,7 @@ export type Data = {
 
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
+
+// user
+export type IUserInput = z.infer<typeof UserInputSchema>
+export type IUserSignIn = z.infer<typeof UserSignInSchema>
