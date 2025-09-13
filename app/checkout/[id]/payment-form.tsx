7290e9c935cdc5d0172c47e39e66bbcd7d0385a4
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-09-13 17:40:57 +0530

    20 Stripe added. But cannot register unless I have a business in India

--- app/checkout/[id]/payment-form.tsx
+++ app/checkout/[id]/payment-form.tsx
@@ -19,13 +19,19 @@ import { redirect, useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import ProductPrice from "@/components/shared/product/ProductPrice"
 
+import StripeForm from './stripe-form'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+
 export default function OrderPaymentForm({
   order,
   paypalClientId,
+  clientSecret,
 }: {
   order: IOrder
   paypalClientId: string
   isAdmin: boolean
+  clientSecret: string | null
 }) {
   const router = useRouter()
   const {
@@ -119,6 +125,20 @@ export default function OrderPaymentForm({
               </div>
             )}
 
+            {!isPaid && paymentMethod === 'Stripe' && clientSecret && (
+              <Elements
+                options={{
+                  clientSecret,
+                }}
+                stripe={stripePromise}
+              >
+                <StripeForm
+                  priceInCents={Math.round(order.totalPrice * 100)}
+                  orderId={order._id}
+                />
+              </Elements>
+            )}
+
             {!isPaid && paymentMethod === 'Cash On Delivery' && (
               <Button
                 className='w-full rounded-full'
@@ -131,6 +151,10 @@ export default function OrderPaymentForm({
         </div>
       </CardContent>
     </Card>
+  )
+
+  const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
   )
 
   return (
