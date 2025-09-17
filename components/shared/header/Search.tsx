15b1a786aea622d3508714449987bf2c9fa38310
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-09-17 11:19:56 +0530

    25 Search Page and Categories page created

--- components/shared/header/Search.tsx
+++ components/shared/header/Search.tsx
@@ -9,10 +9,13 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 import { APP_NAME } from "@/lib/constants"
+import { getAllCategories } from '@/lib/actions/product.actions'
 
-const categories = ["men", "women", "kids", "accessories"];
 
-const Search = () => {
+const Search = async () => {
+
+  const categories = await getAllCategories();
+
   return (
     <form action="/search" method="GET" className="flex items-center h-10">
       <Select name="category">
